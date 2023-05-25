abf3cd5f0444dce35838d213c3c099c88b70da7d
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-05-25 21:12:43 +0530

    rectifying errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,23 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="favicon.svg" type="image/svg+xml" sizes='any'/>
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EFHM3M3LMZ"></script>
-        <script>{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <script
+          data-hydration="defer"
+          src="https://www.googletagmanager.com/gtag/js?id=G-EFHM3M3LMZ"
+          defer
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EFHM3M3LMZ');
+            `,
+          }}
+          defer
+        ></script>
 
-          gtag('config', 'G-EFHM3M3LMZ');
-          `}
-        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
