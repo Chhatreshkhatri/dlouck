68f58ce50ad1b28063cded31179a70653275e682
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-05-25 20:39:14 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="favicon.svg" type="image/svg+xml" sizes='any'/>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EFHM3M3LMZ"></script>
+        <script>{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EFHM3M3LMZ');
+          `}
+        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
