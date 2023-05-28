ab2c040ec101f6dd6523eda3fdf0953d0590cea7
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-05-28 20:09:08 +0530

    keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: "Dlouck | Home",
   description: "Dlouck website built with Next.js and TailwindCSS",
+  keywords: ['Dlouck','Next.js', 'React', 'JavaScript','Github','website'],
   url: "https://www.dlouck.com",
   sitename: "Dlouck",
   type: "website",
