4a3a5b604aaed80048df03201c126811e9b0ecb2
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-05-28 18:16:49 +0530

    layout meta>page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,39 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: "Dlouck | Home",
+  description: "Dlouck website built with Next.js and TailwindCSS",
+  url: "https://www.dlouck.com",
+  sitename: "Dlouck",
+  type: "website",
+  author: "Dlouck",
+  alternates: {
+    canonical: "https://www.dlouck.com",
+  },
+  category: "website",
+  icons: {
+    icon: [{url: '/favicon.svg', sizes: 'any', type: 'image/svg+xml'}],
+    shortcut: [{url: '/favicon.ico', sizes: '16x16 32x32', type: 'image/x-icon'}],
+  },
+  manifest: "/app.webmanifest",
+  openGraph: {
+    title: "Dlouck | Home",
+    description: "Dlouck website built with Next.js and TailwindCSS",
+    url: "https://www.dlouck.com",
+    type: "website",
+    site_name: "Dlouck",
+    author: "Dlouck",
+  },
+  twitter: {
+    title: "Dlouck | Home",
+    url: "https://www.dlouck.com",
+    description: "Dlouck website built with Next.js and TailwindCSS",
+    card: "summary",
+    creator: "@mackXsun",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
