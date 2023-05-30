2b168db075188575b858effd78e7055c96425558
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-05-30 22:46:20 +0530

    testing about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "Dlouck | About",
+    description: "About page for dlouck.com",
+    };
+export default function Home() {
+    return (
+      <main>
+        <div className="flex justify-center pt-24">
+          <h1>About page</h1>
+        </div>
+      </main>
+    );
+  }
