049b7c3f488bcfe6c7a996ad782d5e2b0dacdcb8
Chhatresh <chhatresh.khatri1002@gmail.com>
2023-06-02 09:00:17 +0530

    rm

--- app/robots.tsx
+++ app/robots.tsx
@@ -1,11 +1,11 @@
 import { MetadataRoute } from 'next';
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: '',
     },
     sitemap: 'https://www.dlouck.com/sitemap.xml',
   };
